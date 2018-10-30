615c48e6647fc668802aac0a0e5c30a2cc7e7fa2
egor5q <31898567+egor5q@users.noreply.github.com>
2018-10-30 19:17:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,17 +26,18 @@ parameters={'rows':'10'}
 h=s.get('https://edge.qiwi.com/payment-history/v1/persons/'+str(mylogin)+'/payments', params = parameters) 
 print(json.loads(h.text))
 
-#hook=s.put("https://edge.qiwi.com/payment-notifier/v1/hooks?hookType=1&param=http%3A%2F%2Fecho.fjfalcon.ru%2F&txnType=0")
+#hook=s.put("https://edge.qiwi.com/payment-notifier/v1/hooks?hookType=1&param=http%3A%2F%2Fecho.fjfalcon.ru%2F&txnType=2")
 info=s.get("https://edge.qiwi.com/payment-notifier/v1/hooks/active")
 print(json.loads(info.text))
 tst=s.get('https://edge.qiwi.com/payment-notifier/v1/hooks/test')
 print(json.loads(tst.text))
-
+s.delete('fa6d8174-b2fe-425f-b52d-16d035b1e4c0')
 
 def posts():
-   while True:
       post=s.post('http://echo.fjfalcon.ru/')
       print(json.loads(post.text))
+      t=threading.Timer(10,posts)
+      t.start()
 
 if True:
    t=threading.Timer(1,posts)
